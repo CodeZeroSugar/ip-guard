b30cabe56871cd9120385e815a68925711403595
Ian Velder <icvelder@gmail.com>
2026-02-15 17:17:58 -0600

    created basic client to query a single ip address

--- main.go
+++ main.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
-type getIP struct {
-	IPAddress string `json:"ipAddress"`
-	MaxAgeInDays int `json:"maxAgeInDays"`
-}
-
-type headers
+const TEST_IP = "147.185.132.120"
 
 func main() {
 	godotenv.Load()
 	apiURL := os.Getenv("API_URL")
 	apiKey := os.Getenv("ABUSEIP_KEY")
+
+	checkURL := apiURL + "/check"
+
+	params := url.Values{}
+	params.Add("ipAddress", TEST_IP)
+	params.Add("maxAgeInDays", "90")
+
+	fullURL := fmt.Sprintf("%s?%s", checkURL, params.Encode())
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Key", apiKey)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("%s", err)
+	}
+
+	fmt.Println(resp)
 }
